2e6ab93ee19bfc4dd95c61edfdc9223c44895944
Colt Steele <colt@steele.com>
2019-12-16 13:02:48 -0800

    add v1 of color script

---
+++ app.js
@@ -0,0 +1,16 @@
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`
+}
+
+
+
+// To change each letter independently
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function () {
+  for (let letter of letters) {
+    letter.style.color = randomRGB();
+  }
+}, 2000);
