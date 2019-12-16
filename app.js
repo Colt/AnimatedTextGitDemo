b3901b6b3cfc08a113d9984d02ba552a726e7998
Colt Steele <colt@steele.com>
2019-12-16 15:33:27 -0800

    restrict colors to purples

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function randomRGB() {
   const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
+  const g = 0;
   const b = Math.floor(Math.random() * 256);
   return `rgb(${r},${g},${b})`
 }
